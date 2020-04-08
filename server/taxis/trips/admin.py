79b22ee7045cf01e3eeb12b880a61065c833ca5a
Michael Kim <miketkim@umich.edu>
2020-04-08 01:39:49 -0700

    setup

---
+++ server/taxis/trips/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+from .models import User
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+   pass
+
+
+# Register your models here.
